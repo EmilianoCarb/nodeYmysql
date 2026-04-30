1541fce0921796154be987cdf708323109b8d14b
EmilianoCarb <acarbajal2@ucol.mx>
2026-04-30 08:22:15 -0600

    Cambios router

--- routes/router.js
+++ routes/router.js
@@ -49,7 +49,57 @@ router.post('/sign-up', userMiddleware.validateRegister, (req, res, next) => {
   );
 });
 
-router.post('/login', (req, res, next) => {});
+router.post('/login', (req, res, next) => {
+  db.query(
+    `SELECT * FROM users WHERE username = ?;`,
+    [req.body.username],
+    (err, result) => {
+      if (err) {
+        return res.status(400).send({
+          message: err,
+        });
+      }
+      if (!result.length) {
+        return res.status(400).send({
+          message: 'Username or password incorrect!',
+        });
+      }
+      bcrypt.compare(
+        req.body.password,
+        result[0]['password'],
+        (bErr, bResult) => {
+          if (bErr) {
+            return res.status(400).send({
+              message: 'Username or password incorrect!',
+            });
+          }
+          if (bResult) {
+            // password match
+            const token = jwt.sign(
+              {
+                username: result[0].username,
+                userId: result[0].id,
+              },
+              'SECRETKEY',
+              { expiresIn: '7d' }
+            );
+            db.query(`UPDATE users SET last_login = now() WHERE id = ?;`, [
+              result[0].id,
+            ]);
+            return res.status(200).send({
+              message: 'Logged in!',
+              token,
+              user: result[0],
+            });
+          }
+          return res.status(400).send({
+            message: 'Username or password incorrect!',
+          });
+        }
+      );
+    }
+  );
+});
 
 router.get('/secret-route', (req, res, next) => {
   res.send('This is the secret content. Only logged in users can see that!');
